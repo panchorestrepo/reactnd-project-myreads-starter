89c12079173a5e49f1746d905d3993e5706aea12
panchorestrepo <panchorestrepo@yahoo.com>
2017-07-31 20:31:31 -0500

    Create ChangeShelf.js

---
+++ src/ChangeShelf.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class ChangeShelf extends Component {
+  const { book, changeShelf } = this.props;
+  render() {
+    return (
+            <div className="book-shelf-changer">
+              <select>
+                <option value="none" disabled>Move to...</option>
+                <option value="currentlyReading">Currently Reading</option>
+                <option value="wantToRead">Want to Read</option>
+                <option value="read">Read</option>
+                <option value="none">None</option>
+              </select>
+            </div>
+          )
+  }
+}
