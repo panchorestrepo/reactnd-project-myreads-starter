d417417b9b4fd68c927ba84a1fb913f0b84c3ae4
panchorestrepo <panchorestrepo@yahoo.com>
2017-07-31 20:14:28 -0500

    Create BooksShelf.js
    Book shelf component to be used to show all three shelves

---
+++ src/BooksShelf.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+
+class BooksShelf extends Component {
+  const { shelf, changeShelf } = this.props;
+  render() {
+  }
+}
+
+export default BooksShelf;
