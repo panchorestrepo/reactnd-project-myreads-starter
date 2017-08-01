e97f29de06f21398630d5349829ecc447aceef4f
panchorestrepo <panchorestrepo@yahoo.com>
2017-07-31 20:38:30 -0500

    Update ChangeShelf.js

--- src/ChangeShelf.js
+++ src/ChangeShelf.js
@@ -7,9 +7,9 @@ class ChangeShelf extends Component {
             <div className="book-shelf-changer">
               <select>
                 <option value="none" disabled>Move to...</option>
-                <option value="currentlyReading">Currently Reading</option>
-                <option value="wantToRead">Want to Read</option>
-                <option value="read">Read</option>
+                <option value="currentlyReading" onclick={changeShelf(book,"currentlyReading")}>Currently Reading</option>
+                <option value="wantToRead" onclick={changeShelf(book,"wantToRead")}>Want to Read</option>
+                <option value="read" onclick={changeShelf(book,"read")}>Read</option>
                 <option value="none">None</option>
               </select>
             </div>
