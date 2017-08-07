f00d418e7bc37b5de987faccb4b1b65ccd8ad7cd
Francisco Restrepo <panchorestrepo@hotmail.com>
2017-08-06 23:55:03 -0500

    Delete debugging infor

--- src/Book.js
+++ src/Book.js
@@ -9,14 +9,12 @@ class Book extends Component {
     return (
         <li>
             <div className="book" >
-            <div className="book-top">
-                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
-                <ChangeShelf book={book} changeShelf={changeShelf}/>
-            </div>
-            <div className="book-title">{book.title}</div>
-            <div className="book-authors">{book.authors}</div>
-            <div>Shelf {book.shelf}</div>
-            <div>id {book.id}</div>
+                <div className="book-top">
+                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                    <ChangeShelf book={book} changeShelf={changeShelf}/>
+                </div>
+                <div className="book-title">{book.title}</div>
+                <div className="book-authors">{book.authors}</div>
             </div>
         </li>
     )
